10fc73bdb5c1f6e02bea1199c2d10019af803f9c
Leo Toivio <leo@toivio.se>
2017-10-19 15:28:29 +0200

    Created class for session data

--- Assets/_game/Scripts/GameManager.cs
+++ Assets/_game/Scripts/GameManager.cs
@@ -4,25 +4,43 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
+public class SessionData
+{
+	public int pickupsCollected;
+	public float gameEndTime;
+}
+
 public class GameManager : MonoBehaviour {
 
 	public TMP_Text gameTimer;
-	float timeToEndGame;
 	PickupManager pickupManager;
 
+	SessionData sessionData;
+
 	// Use this for initialization
-	void Start () 
+	void Start ()
 	{
 		pickupManager = GetComponent<PickupManager>();
+		StartGame();
+	}
 
-		timeToEndGame = Time.time + 60f;
+	private void StartGame()
+	{
+		sessionData = new SessionData
+		{
+			gameEndTime = Time.time + 60f
+		};
+
 		pickupManager.StartSpawning();
 	}
-	
+
 	// Update is called once per frame
 	void Update () 
 	{
-		gameTimer.SetText( SplashJam.Utils.GetSecondsAndMinutesText( timeToEndGame - Time.time, 20f ) );
+		if( sessionData == null)
+			return;
+		
+		gameTimer.SetText( SplashJam.Utils.GetSecondsAndMinutesText( sessionData.gameEndTime - Time.time, 20f ) );
 	}
 
 
