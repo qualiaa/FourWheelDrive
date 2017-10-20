fc0a37a703df629dbc95f0ba5828627d1b131413
Jamie Bayne <jamiebayne@hotmail.com>
2017-10-20 23:10:04 +0100

    Add music script

---
+++ Assets/_game/Scripts/MusicScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicScript : MonoBehaviour {
+
+    public AudioSource music;
+    public AudioClip musicLoop;
+
+	// Use this for initialization
+    void Start () {
+        music.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!music.isPlaying)
+        {
+            music.clip = musicLoop;
+            music.loop = true;
+            music.Play();
+        }
+	}
+}
